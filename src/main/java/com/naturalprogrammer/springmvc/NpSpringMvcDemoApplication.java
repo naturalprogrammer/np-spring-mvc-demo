73d07982eea684ae5e5369d5bf15f1c500294afd
Sanjay Patel <skpatel20@gmail.com>
2023-01-13 08:57:14 +0530

    First commit after generating project using Spring Initializer on IntelliJ

---
+++ src/main/java/com/naturalprogrammer/springmvc/NpSpringMvcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.naturalprogrammer.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpSpringMvcDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NpSpringMvcDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/naturalprogrammer/springmvc/NpSpringMvcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naturalprogrammer.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpSpringMvcDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
