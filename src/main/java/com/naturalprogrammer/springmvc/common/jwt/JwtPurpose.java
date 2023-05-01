57d69814f81b4bec667977f312a985f2d9bf1b98
Sanjay Patel <skpatel20@gmail.com>
2023-05-01 10:13:38 +0530

    #4: Send verification mail for changing email

--- src/main/java/com/naturalprogrammer/springmvc/common/jwt/JwtPurpose.java
+++ src/main/java/com/naturalprogrammer/springmvc/common/jwt/JwtPurpose.java
@@ -4,6 +4,7 @@ public enum JwtPurpose {
 
     AUTH,
     EMAIL_VERIFICATION,
+    EMAIL_CHANGE,
     FORGOT_PASSWORD;
 
     public static final String PURPOSE = "purpose";
--- src/main/java/com/naturalprogrammer/springmvc/user/features/change_mail/EmailChangeRequestProcessor.java
+++ src/main/java/com/naturalprogrammer/springmvc/user/features/change_mail/EmailChangeRequestProcessor.java
@@ -1,7 +1,12 @@
 package com.naturalprogrammer.springmvc.user.features.change_mail;
 
 import com.naturalprogrammer.springmvc.common.CommonUtils;
+import com.naturalprogrammer.springmvc.common.MessageGetter;
 import com.naturalprogrammer.springmvc.common.error.*;
+import com.naturalprogrammer.springmvc.common.jwt.JweService;
+import com.naturalprogrammer.springmvc.common.mail.MailData;
+import com.naturalprogrammer.springmvc.common.mail.MailSender;
+import com.naturalprogrammer.springmvc.config.MyProperties;
 import com.naturalprogrammer.springmvc.user.domain.User;
 import com.naturalprogrammer.springmvc.user.repositories.UserRepository;
 import com.naturalprogrammer.springmvc.user.services.UserService;
@@ -11,10 +16,18 @@ import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.time.Clock;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
+import static com.naturalprogrammer.springmvc.common.jwt.JwtPurpose.EMAIL_CHANGE;
+import static com.naturalprogrammer.springmvc.common.jwt.JwtPurpose.PURPOSE;
+import static com.naturalprogrammer.springmvc.user.features.verification.VerificationMailSender.VERIFICATION_TOKEN_VALID_DAYS;
 import static org.apache.commons.lang3.ObjectUtils.notEqual;
+import static org.springframework.security.oauth2.core.oidc.StandardClaimNames.EMAIL;
 
 @Slf4j
 @Service
@@ -46,6 +59,7 @@ class ValidatedEmailChangeRequestProcessor {
     private final UserService userService;
     private final ObjectFactory<ProblemBuilder> problemComposer;
     private final PasswordEncoder passwordEncoder;
+    private final NewEmailVerificationMailSender newEmailVerificationMailSender;
 
     public Optional<Problem> process(UUID userId, UserEmailChangeRequest request) {
 
@@ -79,9 +93,44 @@ class ValidatedEmailChangeRequestProcessor {
         }
 
         user.setNewEmail(request.newEmail());
+        newEmailVerificationMailSender.send(user);
         userRepository.save(user);
 
         log.info("Processed email change request user {}: {}", user, request);
         return Optional.empty();
+    }
+}
+
+@Service
+@RequiredArgsConstructor
+class NewEmailVerificationMailSender {
+
+    private final JweService jweService;
+    private final Clock clock;
+    private final MessageGetter messageGetter;
+    private final MailSender mailSender;
+    private final MyProperties properties;
+
+    public void send(User user) {
+        var verificationToken = createVerificationToken(user);
+        var mail = new MailData(
+                user.getEmail(),
+                messageGetter.getMessage("verification-mail-subject"),
+                messageGetter.getMessage("verification-mail-body",
+                        user.getDisplayName(), properties.homepage(), verificationToken),
+                null
+        );
+        mailSender.send(mail);
+    }
+
+    private String createVerificationToken(User user) {
+        return jweService.createToken(
+                user.getIdStr(),
+                Date.from(clock.instant().plus(VERIFICATION_TOKEN_VALID_DAYS, ChronoUnit.DAYS)),
+                Map.of(
+                        PURPOSE, EMAIL_CHANGE,
+                        EMAIL, user.getNewEmail()
+                )
+        );
     }
 }
