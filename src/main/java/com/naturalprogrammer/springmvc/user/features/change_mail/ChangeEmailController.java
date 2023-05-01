adbda813986cc1595875a3c7d61ac160f8b350f1
Sanjay Patel <skpatel20@gmail.com>
2023-05-01 12:12:00 +0530

    #4: Verify token and change email

--- src/main/java/com/naturalprogrammer/springmvc/user/features/change_mail/ChangeEmailController.java
+++ src/main/java/com/naturalprogrammer/springmvc/user/features/change_mail/ChangeEmailController.java
@@ -1,6 +1,7 @@
 package com.naturalprogrammer.springmvc.user.features.change_mail;
 
 import com.naturalprogrammer.springmvc.common.error.Problem;
+import com.naturalprogrammer.springmvc.user.services.UserResource;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -10,10 +11,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.naturalprogrammer.springmvc.common.Path.USER;
 
@@ -25,6 +23,7 @@ import static com.naturalprogrammer.springmvc.common.Path.USER;
 class ChangeEmailController {
 
     private final EmailChangeRequestProcessor emailChangeRequestProcessor;
+    private final EmailChanger emailChanger;
 
     @Operation(summary = "Request changing email")
     @ApiResponses(value = {
@@ -45,11 +44,40 @@ class ChangeEmailController {
                             schema = @Schema(implementation = Problem.class))
             )
     })
-    @PostMapping(value = "/email", consumes = UserEmailChangeRequest.CONTENT_TYPE)
+    @PostMapping(value = "/email-change-request", consumes = UserEmailChangeRequest.CONTENT_TYPE)
     public ResponseEntity<?> requestChangingEmail(@RequestBody UserEmailChangeRequest request) {
         return emailChangeRequestProcessor
                 .process(request)
                 .map(Problem::toResponse)
                 .orElseGet(() -> ResponseEntity.noContent().build());
     }
+
+    @Operation(summary = "Verify new email and update")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Email changed. Old auth tokens invalidated"),
+            @ApiResponse(responseCode = "422", description = "Invalid input",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                            schema = @Schema(implementation = Problem.class))
+            ),
+            @ApiResponse(responseCode = "403", description = "Token verification failed",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                            schema = @Schema(implementation = Problem.class))
+            ),
+            @ApiResponse(responseCode = "409", description = "New email already used",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_PROBLEM_JSON_VALUE,
+                            schema = @Schema(implementation = Problem.class))
+            )
+    })
+    @PatchMapping(value = "/email-change-request",
+            consumes = UserEmailChangeVerificationRequest.CONTENT_TYPE,
+            produces = UserResource.CONTENT_TYPE)
+    public ResponseEntity<?> changeEmail(@RequestBody UserEmailChangeVerificationRequest request) {
+        return emailChanger.changeEmail(request)
+                .map(Problem::toResponse)
+                .orElseGet(() -> ResponseEntity.noContent().build());
+    }
+
 }
---
+++ src/main/java/com/naturalprogrammer/springmvc/user/features/change_mail/EmailChanger.java
@@ -0,0 +1,88 @@
+package com.naturalprogrammer.springmvc.user.features.change_mail;
+
+import com.naturalprogrammer.springmvc.common.CommonUtils;
+import com.naturalprogrammer.springmvc.common.error.*;
+import com.naturalprogrammer.springmvc.common.jwt.JweService;
+import com.naturalprogrammer.springmvc.user.domain.User;
+import com.naturalprogrammer.springmvc.user.repositories.UserRepository;
+import com.nimbusds.jwt.JWTClaimsSet;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.stereotype.Service;
+
+import java.time.Clock;
+import java.util.Optional;
+import java.util.UUID;
+
+import static com.naturalprogrammer.springmvc.common.jwt.JwtPurpose.EMAIL_CHANGE;
+import static com.naturalprogrammer.springmvc.common.jwt.JwtPurpose.PURPOSE;
+import static org.apache.commons.lang3.ObjectUtils.notEqual;
+import static org.springframework.security.oauth2.core.oidc.StandardClaimNames.EMAIL;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+class EmailChanger {
+
+    private final CommonUtils commonUtils;
+    private final BeanValidator validator;
+    private final ValidatedEmailChanger validatedEmailChanger;
+
+    public Optional<Problem> changeEmail(UserEmailChangeVerificationRequest request) {
+
+        var userId = commonUtils.getUserId().orElseThrow();
+        log.info("Changing email for user {} with {}", userId, request);
+        return validator
+                .validate(request)
+                .or(() -> validatedEmailChanger.changeEmail(userId, request));
+    }
+}
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+class ValidatedEmailChanger {
+
+    private final UserRepository userRepository;
+    private final JweService jweService;
+    private final ObjectFactory<ProblemBuilder> problemComposer;
+    private final Clock clock;
+
+    public Optional<Problem> changeEmail(UUID userId, UserEmailChangeVerificationRequest request) {
+
+        var user = userRepository.findById(userId).orElseThrow();
+
+        return jweService
+                .parseToken(request.emailVerificationToken())
+                .fold(
+                        problemType -> Optional.of(problemComposer.getObject()
+                                .type(problemType)
+                                .detail(request.emailVerificationToken())
+                                .error("emailVerificationToken", ErrorCode.TOKEN_VERIFICATION_FAILED)
+                                .build()),
+                        claims -> changeEmail(user, claims)
+                );
+
+    }
+
+    private Optional<Problem> changeEmail(User user, JWTClaimsSet claims) {
+
+        if (notEqual(claims.getSubject(), user.getIdStr()) ||
+                notEqual(claims.getClaim(PURPOSE), EMAIL_CHANGE.name()) ||
+                notEqual(claims.getClaim(EMAIL), user.getNewEmail()))
+            return Optional.of(problemComposer.getObject().build(ProblemType.TOKEN_VERIFICATION_FAILED, user.toString()));
+
+        if (userRepository.existsByEmail(user.getNewEmail()))
+            return Optional.of(problemComposer.getObject()
+                    .type(ProblemType.USED_EMAIL)
+                    .detailMessage("used-given-email", user.getNewEmail())
+                    .build());
+
+        user.setEmail(user.getNewEmail());
+        user.setNewEmail(null);
+        user.resetTokensValidFrom(clock);
+        userRepository.save(user);
+        return Optional.empty();
+    }
+}
