662d632ba9bb7a4f50e0efb6cd945e122c688789
Sanjay Patel <skpatel20@gmail.com>
2023-06-06 10:39:13 +0530

    Configuration for deploying to azure app service
    Tested locally by giving the following commands:
    
    az group create --name rg-np-spring-mvc-demo --location eastus
    mvn com.microsoft.azure:azure-webapp-maven-plugin:2.11.0:config
    mvn clean package azure-webapp:deploy

--- src/main/java/com/naturalprogrammer/springmvc/common/mail/LoggingMailSender.java
+++ src/main/java/com/naturalprogrammer/springmvc/common/mail/LoggingMailSender.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @Slf4j
 @Component
-@Profile({"default", "test"})
+@Profile({"default", "test", "azure-staging", "azure-live"})
 public class LoggingMailSender implements MailSender {
 
     private static final List<MailData> SENT_MAILS = new ArrayList<>();
